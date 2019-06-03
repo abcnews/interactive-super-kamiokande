6360fac606400d24bda60e619f18b01969eb7dd5
Colin Gourlay <Gourlay.Colin@abc.net.au>
2019-06-03 10:16:18 +1000

    WIP

--- src/index.js
+++ src/index.js
@@ -6,67 +6,112 @@ import SuperK from './components/SuperK';
 import Supernova from './components/Supernova';
 import styles from './styles.css';
 
-let superkScrollyteller;
-let supernovaScrollyteller;
-let assets;
-let schedulerReference;
+const superk = {
+  scrollyteller: null,
+  assets: null,
+  schedulerReference: null
+};
+const superk2 = {
+  scrollyteller: null,
+  assets: null,
+  schedulerReference: null
+};
+const supernova = {
+  scrollyteller: null
+};
 
 function renderSuperK() {
-  if (schedulerReference) {
-    window.__ODYSSEY__.scheduler.unsubscribe(schedulerReference);
+  if (superk.schedulerReference) {
+    window.__ODYSSEY__.scheduler.unsubscribe(superk.schedulerReference);
   }
 
   render(
-    <SuperK assets={assets} panels={superkScrollyteller.panels} />,
-    superkScrollyteller.mountNode,
-    superkScrollyteller.mountNode.firstChild
+    <SuperK assets={superk.assets} panels={superk.scrollyteller.panels} />,
+    superk.scrollyteller.mountNode,
+    superk.scrollyteller.mountNode.firstChild
   );
 
   let mountNodeOpacity = null;
 
-  schedulerReference = client => {
-    const box = superkScrollyteller.mountNode.getBoundingClientRect();
+  superk.schedulerReference = client => {
+    const box = superk.scrollyteller.mountNode.getBoundingClientRect();
     const nextMountNodeOpacity = Math.max(0, Math.min(box.bottom - client.height, client.height)) / client.height;
 
     if (mountNodeOpacity !== nextMountNodeOpacity) {
       mountNodeOpacity = nextMountNodeOpacity;
-      superkScrollyteller.mountNode.style.opacity = mountNodeOpacity;
+      superk.scrollyteller.mountNode.style.opacity = mountNodeOpacity;
     }
   };
 
-  window.__ODYSSEY__.scheduler.subscribe(schedulerReference);
+  window.__ODYSSEY__.scheduler.subscribe(superk.schedulerReference);
+}
+
+function renderSuperK2() {
+  if (superk2.schedulerReference) {
+    window.__ODYSSEY__.scheduler.unsubscribe(superk2.schedulerReference);
+  }
+
+  render(
+    <SuperK assets={superk2.assets} panels={superk2.scrollyteller.panels} />,
+    superk2.scrollyteller.mountNode,
+    superk2.scrollyteller.mountNode.firstChild
+  );
+
+  let mountNodeOpacity = null;
+
+  superk2.schedulerReference = client => {
+    const box = superk2.scrollyteller.mountNode.getBoundingClientRect();
+    const nextMountNodeOpacity = Math.max(0, Math.min(box.bottom - client.height, client.height)) / client.height;
+
+    if (mountNodeOpacity !== nextMountNodeOpacity) {
+      mountNodeOpacity = nextMountNodeOpacity;
+      superk2.scrollyteller.mountNode.style.opacity = mountNodeOpacity;
+    }
+  };
+
+  window.__ODYSSEY__.scheduler.subscribe(superk2.schedulerReference);
 }
 
 function renderSupernova() {
   render(
-    <Supernova panels={supernovaScrollyteller.panels} />,
-    supernovaScrollyteller.mountNode,
-    supernovaScrollyteller.mountNode.firstChild
+    <Supernova panels={supernova.scrollyteller.panels} />,
+    supernova.scrollyteller.mountNode,
+    supernova.scrollyteller.mountNode.firstChild
   );
 }
 
 if (module.hot) {
+  function renderErrorBox(err, instance) {
+    import('./components/ErrorBox').then(exports => {
+      const ErrorBox = exports.default;
+      render(<ErrorBox error={err} />, instance.scrollyteller.mountNode, instance.scrollyteller.mountNode.firstChild);
+    });
+  }
+
   module.hot.accept('./components/SuperK', () => {
-    try {
-      renderSuperK();
-    } catch (err) {
-      import('./components/ErrorBox').then(exports => {
-        const ErrorBox = exports.default;
-        render(<ErrorBox error={err} />, superkScrollyteller.mountNode, superkScrollyteller.mountNode.firstChild);
-      });
+    if (superk.scrollyteller) {
+      try {
+        renderSuperK();
+      } catch (err) {
+        renderErrorBox(err, superk);
+      }
+    }
+
+    if (superk2.scrollyteller) {
+      try {
+        renderSuperK2();
+      } catch (err) {
+        renderErrorBox(err, superk2);
+      }
     }
   });
-}
-
-if (module.hot) {
   module.hot.accept('./components/Supernova', () => {
-    try {
-      renderSupernova();
-    } catch (err) {
-      import('./components/ErrorBox').then(exports => {
-        const ErrorBox = exports.default;
-        render(<ErrorBox error={err} />, supernovaScrollyteller.mountNode, supernovaScrollyteller.mountNode.firstChild);
-      });
+    if (supernova.scrollyteller) {
+      try {
+        renderSupernova();
+      } catch (err) {
+        renderErrorBox(err, supernova);
+      }
     }
   });
 }
@@ -75,18 +120,39 @@ function init() {
   // SuperK
 
   try {
-    superkScrollyteller = loadOdysseyScrollyteller('superk', cn('u-full', styles.mountNode));
+    superk.scrollyteller = loadOdysseyScrollyteller('superk', cn('u-full', styles.mountNode));
   } catch (e) {}
 
-  if (superkScrollyteller && superkScrollyteller.mountNode) {
-    while (superkScrollyteller.mountNode.nextElementSibling.tagName === 'A') {
-      window.__ODYSSEY__.utils.dom.detach(superkScrollyteller.mountNode.nextElementSibling);
+  if (superk.scrollyteller && superk.scrollyteller.mountNode) {
+    while (superk.scrollyteller.mountNode.nextElementSibling.tagName === 'A') {
+      window.__ODYSSEY__.utils.dom.detach(superk.scrollyteller.mountNode.nextElementSibling);
     }
 
-    fetchAssets(superkScrollyteller.panels)
+    fetchAssets(superk.scrollyteller.panels)
       .then(_assets => {
-        assets = _assets;
+        superk.assets = _assets;
         renderSuperK();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  // SuperK 2
+
+  try {
+    superk2.scrollyteller = loadOdysseyScrollyteller('superk2', cn('u-full', styles.mountNode));
+  } catch (e) {}
+
+  if (superk2.scrollyteller && superk2.scrollyteller.mountNode) {
+    while (superk2.scrollyteller.mountNode.nextElementSibling.tagName === 'A') {
+      window.__ODYSSEY__.utils.dom.detach(superk2.scrollyteller.mountNode.nextElementSibling);
+    }
+
+    fetchAssets(superk2.scrollyteller.panels)
+      .then(_assets => {
+        superk2.assets = _assets;
+        renderSuperK2();
       })
       .catch(err => {
         console.error(err);
@@ -96,12 +162,12 @@ function init() {
   // Supernova
 
   try {
-    supernovaScrollyteller = loadOdysseyScrollyteller('supernova', cn('u-full', styles.mountNode));
+    supernova.scrollyteller = loadOdysseyScrollyteller('supernova', cn('u-full', styles.mountNode));
   } catch (e) {}
 
-  if (supernovaScrollyteller && supernovaScrollyteller.mountNode) {
-    while (supernovaScrollyteller.mountNode.nextElementSibling.tagName === 'A') {
-      window.__ODYSSEY__.utils.dom.detach(supernovaScrollyteller.mountNode.nextElementSibling);
+  if (supernova.scrollyteller && supernova.scrollyteller.mountNode) {
+    while (supernova.scrollyteller.mountNode.nextElementSibling.tagName === 'A') {
+      window.__ODYSSEY__.utils.dom.detach(supernova.scrollyteller.mountNode.nextElementSibling);
     }
 
     renderSupernova();
@@ -113,7 +179,7 @@ function init() {
   const flippedMainEl = document.createElement('main');
   let nextNode;
 
-  while ((nextNode = supernovaScrollyteller.mountNode.nextSibling)) {
+  while ((nextNode = supernova.scrollyteller.mountNode.nextSibling)) {
     flippedMainEl.appendChild(nextNode);
   }
 
