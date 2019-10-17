b6efe2b00eefc85c994c8d4dbc7f5dc6c698da21
Colin Gourlay <gourlay.colin@abc.net.au>
2019-10-17 13:29:11 +1000

    Remove unused supanova JS entry-point

--- aunty.config.js
+++ aunty.config.js
@@ -7,9 +7,6 @@ module.exports = {
     hot: false
   },
   webpack: config => {
-    // Add script that runs inside supernova iframe
-    config.entry['supernova'] = [config.entry.index[0].replace('index', 'supernova')];
-
     // Stop `import()`-ed chunks from being split into `[name].js` and `vendors~[name].js`
     config.optimization = config.optimization || {};
     config.optimization.splitChunks = {
